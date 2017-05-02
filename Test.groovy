d80d9b9a9a5ffecdd5bd88e26a30a0220ad9501f
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2017-05-02 10:50:07 +0200

    Test

--- Test.groovy
+++ Test.groovy
@@ -3,9 +3,48 @@
 node('master') {
     timestamps {
         stage('Checkout') {
+            dir('root') {
+                // TODO: Use the git step when it has implemented specifying refspecs
+                //checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [],
+                //        submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
+            }
+
+            dir('roottest') {
+               // git url: 'https://github.com/root-project/roottest.git', branch: ROOTTEST_BRANCH
+            }
+
             dir('rootspi') {
                 git url: 'https://github.com/root-project/rootspi.git'
             }
+        }
+
+        try {
+            stage('Build') {
+                //sh 'rootspi/jenkins/jk-all build'
+            }
+
+            stage('Test') {
+                //sh 'rootspi/jenkins/jk-all test'
+
+                def testThreshold = [[$class: 'FailedThreshold', 
+                        failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', 
+                        unstableThreshold: '0'], [$class: 'SkippedThreshold', failureNewThreshold: '', 
+                        failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']]
+
+                step([$class: 'XUnitBuilder', 
+                        testTimeMargin: '3000', thresholdMode: 1, thresholds: testThreshold, 
+                        tools: [[$class: 'CTestType', 
+                                deleteOutputFiles: true, failIfNotNew: false, pattern: 'build/Testing/*/Test.xml', 
+                                skipNoTestFiles: false, stopProcessingIfError: true]]])
+                
+                if (currentBuild.result == 'FAILURE') {
+                    throw new Exception("Test result caused build to fail")
+                }
+            }
+        } catch (err) {
+            println 'Build failed because:'
+            println err
+            currentBuild.result = 'FAILURE'
         }
 
         stage('Generate reports') {
@@ -13,5 +52,10 @@ node('master') {
                     parsingRulesPath: "${pwd()}/rootspi/jenkins/logparser-rules/ROOT-incremental-LogParserRules.txt", 
                     useProjectRule: false, unstableOnWarning: false, failBuildOnError: true])
         }
+
+        //stage('Archive environment') {
+        // TODO: Bundle and store build env in here
+            //archiveArtifacts artifacts: 'build/'
+        //}
     }
 }
