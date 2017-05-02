53c6c51dbea7984b60ccd82a0472050f54f5eb75
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2017-05-02 10:46:57 +0200

    test

--- Test.groovy
+++ Test.groovy
@@ -1,15 +1,17 @@
+#!groovy
+
 node('master') {
-    
-    dir('rootspi') {
-        git url: 'https://github.com/root-project/rootspi'
+    timestamps {
+        stage('Checkout') {
+            dir('rootspi') {
+                git url: 'https://github.com/root-project/rootspi.git'
+            }
+        }
+
+        stage('Generate reports') {
+            step([$class: 'LogParserPublisher',
+                    parsingRulesPath: "${pwd()}/rootspi/jenkins/logparser-rules/ROOT-incremental-LogParserRules.txt", 
+                    useProjectRule: false, unstableOnWarning: false, failBuildOnError: true])
+        }
     }
-    sh "cat consoleText.txt"
-    sh "echo 'end of log'"
-    
-    
-
-    step([$class: 'LogParserPublisher',
-            parsingRulesPath: "${pwd()}/rootspi/jenkins/logparser-rules/ROOT-incremental-LogParserRules.txt", 
-            useProjectRule: false, unstableOnWarning: false, failBuildOnError: true])
-
 }
