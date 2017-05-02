c340709f3f42a8727bfd82d7da1be2ca962c3945
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2017-05-02 10:54:36 +0200

    Test

--- Test.groovy
+++ Test.groovy
@@ -1,16 +1,38 @@
 #!groovy
 
+properties([
+    parameters([
+        string(name: 'ROOT_REFSPEC', defaultValue: '', description: 'Refspec for ROOT repository'),
+        string(name: 'ROOTTEST_BRANCH', defaultValue: 'master', description: 'Name of the ROOT branch to work with'),
+        string(name: 'ROOT_BRANCH', defaultValue: 'master', description: 'Name of the roottest branch to work with'),
+        string(name: 'BUILD_NOTE', defaultValue: '', description: 'Note to add after label/compiler in job name'),
+        string(name: 'BUILD_DESCRIPTION', defaultValue: '', description: 'Build description')
+    ])
+])
+
+
+// Treat parameters as environment variables
+for (ParameterValue p in params) {
+    env[p.key] = p.value
+}
+
+// TODO: This should be avoided 
+env.GIT_URL = 'https://github.com/root-project/root.git'
+
+currentBuild.setDisplayName("#$BUILD_NUMBER $LABEL/$COMPILER $BUILD_NOTE")
+currentBuild.setDescription("$BUILD_DESCRIPTION")
+
 node('master') {
     timestamps {
         stage('Checkout') {
             dir('root') {
                 // TODO: Use the git step when it has implemented specifying refspecs
-                //checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [],
-                //        submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
+                checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [],
+                        submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
             }
 
             dir('roottest') {
-               // git url: 'https://github.com/root-project/roottest.git', branch: ROOTTEST_BRANCH
+                git url: 'https://github.com/root-project/roottest.git', branch: ROOTTEST_BRANCH
             }
 
             dir('rootspi') {
@@ -35,7 +57,7 @@ node('master') {
                         testTimeMargin: '3000', thresholdMode: 1, thresholds: testThreshold, 
                         tools: [[$class: 'CTestType', 
                                 deleteOutputFiles: true, failIfNotNew: false, pattern: 'build/Testing/*/Test.xml', 
-                                skipNoTestFiles: false, stopProcessingIfError: true]]])
+                                skipNoTestFiles: true, stopProcessingIfError: true]]])
                 
                 if (currentBuild.result == 'FAILURE') {
                     throw new Exception("Test result caused build to fail")
